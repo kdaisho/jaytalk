cf0f4892e40dece15e53db7033664983a4c9db19
Daisho Komiyama <daishokomiyama@gmail.com>
2023-11-07 21:27:42 -0500

    Add apply library function

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -1,8 +1,22 @@
+import { CALL_EXPRESSION } from './parse'
 import Moth from './standard-library'
+import { CallExpression } from './types'
 
 const last = (collection: any[]) => collection.at(-1)
 
+const apply = (node: CallExpression): number => {
+    const fn = Moth[node.name]
+    const args = node.arguments.map(evaluate)
+
+    if (typeof fn !== 'function') {
+        throw new TypeError(`${node.name} is not a function`)
+    }
+
+    return fn(...args)
+}
+
 const evaluate = (node: any) => {
+    if (node.type === CALL_EXPRESSION) return apply(node)
     if (node.value) return node.value
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { CALL_EXPRESSION } from './parse'
 import { NUMBER, PARENTHESIS, STRING, NAME } from './tokenize'
 
 export type NumericToken = { type: typeof NUMBER; value: number }
@@ -7,3 +8,9 @@ export type StringToken = {
 }
 
 export type Token = NumericToken | StringToken
+
+export type CallExpression = {
+    type: typeof CALL_EXPRESSION
+    name: 'add' | 'subtract' | 'multiply' | 'divide' | 'modulo'
+    arguments: Token[]
+}
